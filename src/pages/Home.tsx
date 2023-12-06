908e34b385d56c2ac9193320093d77de06dc27bc
Andrianina24 <rakotozananya5@gmail.com>
2023-12-06 10:09:38 +0300

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,6 @@ import './Home.css';
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
